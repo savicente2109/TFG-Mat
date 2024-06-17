a347035e37a004fe44feebb22b9bf8aeea10c1be
savicente2109 <79699139+savicente2109@users.noreply.github.com>
2024-06-17 12:08:55 +0200

    últimos comentarios

--- GAN.py
+++ GAN.py
@@ -49,47 +49,83 @@ def save_plot(examples, epoch, n=7):
     plt.savefig(filename)
     plt.close()
 
+# analizar el funcionamiento de la red en términos cuantitativos es complicado,
+# y se suelen utilizar técnicas cualitativas: generaremos imágenes de ejemplo tras cada cierto
+# número de epochs y al finalizar seleccionaremos los mejores modelos guardados
+# (según nuestro criterio humano)
 def summarize_performance(epoch, generator, discriminator, dataset, latent_dim, n_samples=150):
-    # prepare real samples
+    # creamos imágenes reales
     X_real, y_real = generate_real_samples(dataset, n_samples)
-    # evaluate discriminator on real examples
+    # evaluamos el discriminador en ellas
     _, acc_real = discriminator.evaluate(X_real, y_real, verbose=0)
-    # prepare fake examples
+    # peparamos imágenes falsas
     x_fake, y_fake = generate_fake_samples(generator, latent_dim, n_samples)
-    # evaluate discriminator on fake examples
+    # evaluamos el discriminador en ellas
     _, acc_fake = discriminator.evaluate(x_fake, y_fake, verbose=0)
-    # summarize discriminator performance
+    # resumen del desempeño del discriminador (accuracy)
     print('>Accuracy real: %.0f%%, fake: %.0f%%' % (acc_real*100, acc_fake*100))
-    # save plot
+    # GRAFICAMOS LAS IMÁGENES GENERADAS POR G
     save_plot(x_fake, epoch)
-    # save the generator model tile file
+    # guardamos el modelo G de la epoch actual (aquí se modifica la ruta)
     filename = '.\generator_models\generator_model_%03d.h5' % (epoch+1)
     generator.save(filename)
 
+# ENTRENAMIENTO COMPLETO DE UNA RED GAN (por defecto 200 epochs y batches de 128 imágenes)
 def train_gan(gan, generator, discriminator, dataset, latent_dim, n_epochs=200, n_batch=128):
+
+    # calculamos el número de batches en cada epoch
     bat_per_epo = int(dataset.shape[0] / n_batch)
+    # la mitad del batch (half_batch) serán reales y la otra mitad, falsas
     half_batch = int(n_batch / 2)
-    # manually enumerate epochs
+
+    # bucle que recorre las epochs
     for i in range(n_epochs):
-    # enumerate batches over the training set
+        
+        # bucle que recorre cada batch del dataset en la epoch i
         for j in range(bat_per_epo):
-            # get randomly selected 'real' samples
+
+            # ACTUALIZAMOS EL DISCRIMINADOR:
+
+            # seleccionamos imágenes reales aleatoriamente
+            # (ya vienen etiquetadas como reales)
             X_real, y_real = generate_real_samples(dataset, half_batch)
-            # update discriminator model weights
+
+            # entrenamos el discriminador con ellas
+            # (aquí sí se actualizan sus pesos, recordamos que era NO ENTRENABLE)
             d_loss1, _ = discriminator.train_on_batch(X_real, y_real)
-            # generate 'fake' examples
+
+            # generamos imágenes falsas con la red generadora
+            # (ya vienen etiquetadas como falsas)
             X_fake, y_fake = generate_fake_samples(generator, latent_dim, half_batch)
-            # update discriminator model weights
+            
+            # entrenamos el discriminador con ellas
+            # (aquí sí se actualizan sus pesos, recordamos que era NO ENTRENABLE)
             d_loss2, _ = discriminator.train_on_batch(X_fake, y_fake)
-            # prepare points in latent space as input for the generator
+
+            # AQUÍ TERMINA EL "BUCLE INTERNO" DEL ALGORITMO DE ENTRENAMIENTO
+            # CLÁSICO DE LAS GAN: ACTUALIZACIÓN DEL DISCRIMINADOR
+            # (con k=1, por eso realmente no es un bucle)
+
+            # AHORA ACTUALIZAMOS EL GENERADOR:
+
+            # generamos puntos del espacio latente (ruido gaussiano)
             X_gan = generate_latent_points(latent_dim, n_batch)
-            # create inverted labels for the fake samples
+
+            # creamos etiquetas "invertidas" (llamamos "reales" a las muestras falsas)
+            # nuestro objetivo es que sea "bueno" que el discriminador "falle"
             y_gan = np.ones((n_batch, 1))
-            # update the generator via the discriminator's error
+
+            # actualizamos el generador con el error del discriminador
+            # (entrenamos la GAN COMPLETA, pero el discriminador no verá sus pesos afectados
+            # porque es NO ENTRENABLE)
             g_loss = gan.train_on_batch(X_gan, y_gan)
-            # summarize loss on this batch
+
+            # resumen del error en este batch (pérdida del discriminador en los datos,
+            # pérdida del discriminador en las falsas,
+            # pérdida (invertida) del discriminador en las falsas)
             print('>%d, %d/%d, d1=%.3f, d2=%.3f g=%.3f' %
             (i+1, j+1, bat_per_epo, d_loss1, d_loss2, g_loss))
+            
         # llamamos a summarize_performance cada 10 epochs,
         # esto se puede modificar aquí
         if (i+1) % 10 == 0:
